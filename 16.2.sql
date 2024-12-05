e5846de91b3f0c9bcf3f02f928869e310a5bbe51
unga <avbjankina@stud.kantiana.ru>
2024-12-05 17:26:13 +0200

    task

---
+++ 16.2.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT
+    pc1.model, 
+    pc2.model, 
+    pc1.speed, 
+    pc1.ram
+FROM 
+    pc pc1
+INNER JOIN 
+    pc pc2 
+ON 
+    pc1.ram = pc2.ram 
+    AND pc1.speed = pc2.speed 
+    AND pc1.model > pc2.model
